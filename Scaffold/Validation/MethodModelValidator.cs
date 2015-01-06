941765d52c0b79e2b60c50a9b6a4919798cedbce
rifai <rifai@microvac.co.id>
2015-01-06 13:28:38 +0700

    Add MethodModelValidator and MethodModelValidatorProvider

---
+++ Scaffold/Validation/MethodModelValidator.cs
@@ -0,0 +1,43 @@
+﻿using FluentValidation;
+using Scaffold.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Validation;
+
+namespace Scaffold.Validation
+{
+    public class MethodModelValidator: ModelValidator
+    {
+        public MethodModelValidator(IEnumerable<ModelValidatorProvider> validatorProviders)
+            : base(validatorProviders)
+        {
+
+        }
+
+        public class FluentValidator<T>: AbstractValidator<T>
+        {
+        }
+
+        public override IEnumerable<ModelValidationResult> Validate(System.Web.Http.Metadata.ModelMetadata metadata, object container)
+        {
+            if (metadata.Model != null &&(metadata.ModelType.IsInstanceOfType(metadata.Model)))
+            {
+                var methods = metadata.ModelType.GetMethods();
+                foreach (var method in methods)
+                {
+                    if (method.CustomAttributes.Any(e => e.AttributeType == typeof(ValidatorAttribute)))
+                    {
+                        var results = (IEnumerable<ModelValidationResult>)method.Invoke(metadata.Model, new object[0]);
+                        foreach (var result in results)
+                        {
+                            yield return result;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Scaffold/Validation/MethodModelValidatorProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Metadata;
+using System.Web.Http.Validation;
+
+namespace Scaffold.Validation
+{
+    public class MethodModelValidatorProvider: ModelValidatorProvider
+    {
+
+        public override IEnumerable<ModelValidator> GetValidators(ModelMetadata metadata, IEnumerable<ModelValidatorProvider> validatorProviders)
+        {
+            if (metadata.Model == null)
+                return new ModelValidator[0];
+            return new ModelValidator[] { new MethodModelValidator(validatorProviders) };
+        }
+    }
+}
